b308da07f4e23fb372e4d348179c6a031bd6f89d
golam mostafa <gmnayeem25@gmail.com>
2022-10-14 22:44:02 +0600

    fix: loading

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { newsCategory } from './news';
 import Header from './components/header';
 import NewsList from './components/newsList';
+import Pagination from './components/pagination';
+import Loading from './components/loading';
 
 const fakeNews = [
   {
@@ -33,7 +35,17 @@ class App extends Component {
         <div className='row'>
           <div className='col-md-6 offset-md-3'>
             <Header category={newsCategory.technology} />
+            <div className='d-flex'>
+              <p className='text-black-50'>
+                About {0} results found
+              </p>
+              <p className='text-black-50' style={{ marginLeft: 'auto' }}>
+                {1} page of {100}
+              </p>
+            </div>
             <NewsList news={fakeNews} />
+            <Pagination />
+            <Loading />
           </div>
         </div>
       </div>
